085956e5476842faa2b9584284cd8b8bc2a71040
Garrett Giordano <ggiordano@phytec.com>
2025-01-08 16:08:47 -0800

    main_menu: add color variations to stars
    Signed-off-by: Garrett Giordano <ggiordano@phytec.com>

--- scripts/main_menu_starfield.gd
+++ scripts/main_menu_starfield.gd
@@ -6,13 +6,15 @@ class Star:
 	var flicker_speed: float
 	var time_offset: float
 	var base_alpha: float
+	var color: Color
 
-	func _init(pos: Vector2, sz: float, speed: float, offset: float, alpha: float):
+	func _init(pos: Vector2, sz: float, speed: float, offset: float, alpha: float, col: Color):
 		position = pos
 		size = sz
 		flicker_speed = speed
 		time_offset = offset
 		base_alpha = alpha
+		color = col
 
 class ShootingStar:
 	var position: Vector2
@@ -37,7 +39,16 @@ var rng = RandomNumberGenerator.new()
 @export var max_star_size: float = 3.0
 @export var min_flicker_speed: float = 1.0
 @export var max_flicker_speed: float = 3.0
-@export var shooting_star_chance: float = 0.02  # Chance per second
+@export var star_colors: Array[Color] = [
+	Color(1.0, 1.0, 1.0),    # White
+	Color(0.9, 0.9, 1.0),    # Slight blue
+	Color(1.0, 0.9, 0.9),    # Slight red
+	Color(1.0, 1.0, 0.9),    # Slight yellow
+	Color(0.9, 1.0, 0.9),    # Slight green
+	Color(1.0, 0.8, 0.6)     # Orange-ish
+]
+@export var color_variation: float = 0.1
+@export var shooting_star_chance: float = 0.02
 @export var shooting_star_speed: float = 500.0
 @export var shooting_star_length: float = 30.0
 @export var shooting_star_lifetime: float = 1.0
@@ -59,7 +70,14 @@ func generate_stars():
 		var offset = rng.randf_range(0, PI * 2)
 		var base_alpha = rng.randf_range(0.5, 1.0)
 
-		stars.append(Star.new(pos, size, speed, offset, base_alpha))
+		var base_color = star_colors[rng.randi() % star_colors.size()]
+		var varied_color = Color(
+			clampf(base_color.r + rng.randf_range(-color_variation, color_variation), 0, 1),
+			clampf(base_color.g + rng.randf_range(-color_variation, color_variation), 0, 1),
+			clampf(base_color.b + rng.randf_range(-color_variation, color_variation), 0, 1)
+		)
+
+		stars.append(Star.new(pos, size, speed, offset, base_alpha, varied_color))
 
 func _process(delta):
 	# Update shooting stars
@@ -71,7 +89,6 @@ func _process(delta):
 		if shooting_star.lifetime <= 0:
 			shooting_stars.remove_at(i)
 
-	# Chance to spawn new shooting star
 	if randf() < shooting_star_chance * delta:
 		spawn_shooting_star()
 
@@ -81,10 +98,10 @@ func spawn_shooting_star():
 	var viewport_size = get_viewport_rect().size
 	var start_pos = Vector2(
 		rng.randf_range(-100, viewport_size.x + 100),
-		-50  # Start above screen
+		-50
 	)
 
-	var angle = rng.randf_range(PI * 0.2, PI * 0.8)  # Diagonal downward trajectory
+	var angle = rng.randf_range(PI * 0.2, PI * 0.8)
 	var velocity = Vector2(cos(angle), sin(angle)) * shooting_star_speed
 
 	shooting_stars.append(ShootingStar.new(
@@ -99,12 +116,13 @@ func _draw():
 	for star in stars:
 		var flicker = (sin(Time.get_ticks_msec() * 0.001 * star.flicker_speed + star.time_offset) + 1) * 0.5
 		var alpha = star.base_alpha * (0.5 + 0.5 * flicker)
-		draw_circle(star.position, star.size, Color(1, 1, 1, alpha))
+		var star_color = star.color
+		star_color.a = alpha
+		draw_circle(star.position, star.size, star_color)
 
 	# Draw shooting stars
 	for shooting_star in shooting_stars:
 		var fade = shooting_star.lifetime / shooting_star.max_lifetime
 		var tail_start = shooting_star.position
 		var tail_end = shooting_star.position - shooting_star.velocity.normalized() * shooting_star.length
-
 		draw_line(tail_start, tail_end, Color(1, 1, 1, fade), 2.0)
